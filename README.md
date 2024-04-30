09f95f7bebbe5c9f5facb8e8174f387a909f2efc
glaucoSapucaia <glaucopco@gmail.com>
2024-04-30 09:48:51 -0300

    CSS style | mosca_alvo IMG | moscaAlvoPosicao() function - Logic eixo x/y | var mosca attrs definition

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mini_game_js
 js project
+
+## Tools
+- [imgs](https://br.freepik.com/)
--- js/app.js
+++ js/app.js
@@ -1,12 +1,37 @@
 // BOM - window
-var altura = null
 var largura = null
+var altura = null
 
 function ajustaTela() {
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+    largura = window.innerWidth
+    altura = window.innerHeight
 
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
 
 ajustaTela()
+
+// posição randomica | mosca alvo
+function moscaAlvoPosicao() {
+    // -90 = reduz eixos da img para não criar efeito crossover, ultrapassando a tela
+    var eixo_x = Math.floor(Math.random() * largura) - 90
+    var eixo_y = Math.floor(Math.random() * altura) - 90
+
+    // agora, evitamos valores negativos, ja que sumirão da tela visivel
+    eixo_x = eixo_x < 0 ? 0 : eixo_x
+    eixo_y = eixo_y < 0 ? 0 : eixo_y
+    
+    console.log(eixo_x, eixo_y)
+    
+    // DOM
+    // cirando e definindo elemento img-HTML
+    var mosca = document.createElement('img')
+    mosca.src = '../img/mosca_sem_fundo.png'
+    mosca.className = 'mosca_alvo'
+    
+    mosca.style.left = eixo_x + 'px'
+    mosca.style.top = eixo_y + 'px'
+    mosca.style.position = 'absolute'
+
+    document.body.appendChild(mosca)
+}

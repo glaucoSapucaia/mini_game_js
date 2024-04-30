893b5cdc208f6c289baf1f078d3c2ffa50a3aafa
glaucoSapucaia <glaucopco@gmail.com>
2024-04-30 09:12:45 -0300

    onresize | ajustaTela() function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-alert('teste')
+// BOM - window
+var altura = null
+var largura = null
+
+function ajustaTela() {
+    var altura = window.innerHeight
+    var largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustaTela()
